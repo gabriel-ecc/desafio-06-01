9d538aa9945a661fba964efee2d897de1d909a3b
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-05-25 14:55:44 -0400

    carga inicial desafio version estable

---
+++ index.js
@@ -0,0 +1,18 @@
+const { mostrarCitas, registrarCita } = require('./operaciones.js')
+
+const args = process.argv.slice(2);
+const comando = args[0];
+
+if(comando === 'registrar'){
+    if (args.length < 5) {
+        console.log('Uso: node index.js registrar <nombre> <edad> <animal> <color> <enfermedad>');
+    }else{
+        registrarCita(args[1], args[2], args[3], args[4], args[5]);
+        console.log('Cita registrada con éxito.')
+    }
+
+} else if (comando === 'leer'){
+    mostrarCitas();
+}else{
+    console.log('Opcion no valida, solo acepta registrar o leer');
+}
---
+++ operaciones.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const _pathName = "citas.json";
+
+const mostrarCitas = () => {
+  console.log("Citas registradas:");
+  const citas = fs.readFileSync(_pathName, "utf8");
+  data = JSON.parse(citas);
+  if (data.length === 0) {
+    console.log("No hay registros para mostrar.");
+  } else {
+    data.forEach((cita) => {
+      console.log(cita);
+    });
+  }
+  console.log("Fin Citas registradas");
+};
+
+const registrarCita = (nombre, edad, animal, color, enfermedad) => {
+  const citas = leerRegistros();
+
+  const newCita = {
+    nombre,
+    edad,
+    animal,
+    color,
+    enfermedad,
+  };
+  citas.push(newCita);
+  guardarRegistros(citas);
+  console.log("Registro guardado exitosamente.");
+};
+
+function leerRegistros() {
+  try {
+    const data = fs.readFileSync(_pathName, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      return []; // Return empty array if file doesn't exist
+    }
+    console.error("Error al leer el archivo de registros:", error);
+    return [];
+  }
+}
+
+function guardarRegistros(citas) {
+    try {
+        fs.writeFileSync(_pathName, JSON.stringify(citas));
+    } catch (error) {
+        console.error('Error al guardar el archivo de registros:', error);
+    }
+}
+
+module.exports = { mostrarCitas, registrarCita };
